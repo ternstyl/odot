6cb654eca7d818d39008aeb2524209d92ab82efe
Marie Veverka <mlvsquirrel@yahoo.com>
2014-08-09 15:55:41 -0700

    Clean up todo mark completion view

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe "Viewing todo items" do
-	let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries")}
+  let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
 
-  it "displays the title of the todo list" do 
+  it "displays the title of the todo list" do
     visit_todo_list(todo_list)
     within("h1") do
       expect(page).to have_content(todo_list.title)
     end
-  end
+  end  
 
-	it "displays no items when a todo list is empty" do
+  it "displays no items when a todo list is empty" do
     visit_todo_list(todo_list)
     expect(page.all("ul.todo_items li").size).to eq(0)
-	end	
+  end
 
   it "displays item content when a todo list has items" do
     todo_list.todo_items.create(content: "Milk")
@@ -23,7 +23,7 @@ describe "Viewing todo items" do
     
     expect(page.all("ul.todo_items li").size).to eq(2)
     
-    within "ul.todo_items" do 
+    within "ul.todo_items" do
       expect(page).to have_content("Milk")
       expect(page).to have_content("Eggs")
     end
