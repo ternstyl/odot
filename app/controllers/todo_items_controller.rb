2d43cb201a9b844d541812ef15f4dea00b63c0a1
Marie Veverka <mlvsquirrel@yahoo.com>
2014-08-09 15:12:40 -0700

    Mark todo item as complete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -5,18 +5,18 @@ class TodoItemsController < ApplicationController
   end
 
   def new
-  	@todo_item = @todo_list.todo_items.new
+    @todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_item = @todo_list.todo_items.new(todo_item_params)
-  	if @todo_item.save
-  		flash[:success] = "Added todo list item."
-  		redirect_to todo_list_todo_items_path
-  	else
-  		flash[:error] = "There was a problem adding that todo list item."
-  		render action: :new
-  	end
+    @todo_item = @todo_list.todo_items.new(todo_item_params)
+    if @todo_item.save
+      flash[:success] = "Added todo list item."
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error] = "There was a problem adding that todo list item."
+      render action: :new
+    end
   end
 
   def edit
@@ -24,7 +24,7 @@ class TodoItemsController < ApplicationController
   end
 
   def update
-    @todo_item = @todo_list.todo_items.new(todo_item_params)
+    @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
       flash[:success] = "Saved todo list item."
       redirect_to todo_list_todo_items_path
@@ -44,7 +44,15 @@ class TodoItemsController < ApplicationController
     redirect_to todo_list_todo_items_path
   end
 
+  def complete
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to todo_list_todo_items_path, notice: "Todo item marked as complete."
+  end
 
+  def url_options
+    { todo_list_id: params[:todo_list_id] }.merge(super)
+  end
 
   private
   def find_todo_list
@@ -52,7 +60,7 @@ class TodoItemsController < ApplicationController
   end
 
   def todo_item_params
-  	params[:todo_item].permit(:content)
+    params[:todo_item].permit(:content)
   end
 
 end
